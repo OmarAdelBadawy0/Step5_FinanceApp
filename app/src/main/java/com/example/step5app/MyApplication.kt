1a99f6b75d86f97e263b6d7ba05d883007cc11d7
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-05-10 21:55:21 +0300

    add HiltApplication file to be the initiator if Hilt object

---
+++ app/src/main/java/com/example/step5app/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.example.step5app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+
+//    override fun onCreate() {
+//        super.onCreate()
+//        // Optional: Add global app initialization code here
+//    }
+}

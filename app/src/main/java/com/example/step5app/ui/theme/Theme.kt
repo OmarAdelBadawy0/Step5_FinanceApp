3cdadbb614b2b8d4fd8756e43e9100c09f9e0d04
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-05-19 19:13:13 +0300

    add the Theme handling in Theme.kt

--- app/src/main/java/com/example/step5app/ui/theme/Theme.kt
+++ app/src/main/java/com/example/step5app/ui/theme/Theme.kt
@@ -43,17 +43,21 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun Step5AppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
+    selectedTheme: String = "system", // "light", "dark", "system"
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
+    val darkTheme = when (selectedTheme.lowercase()) {
+        "dark" -> true
+        "light" -> false
+        else -> isSystemInDarkTheme()
+    }
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }

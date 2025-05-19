ef7e973c862f603b27c4422909632ed07d2c642f
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-05-19 19:34:39 +0300

    modify the MainActivity.kt to handle the Theme swap

--- app/src/main/java/com/example/step5app/MainActivity.kt
+++ app/src/main/java/com/example/step5app/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.example.step5app
 
+import FeedScreen
+import SettingsScreen
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.step5app.presentation.auth.AuthScreen
+import com.example.step5app.presentation.navigation.MainNavHost
+import com.example.step5app.presentation.settings.SettingsViewModel
 import com.example.step5app.ui.theme.Step5AppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import androidx.compose.runtime.getValue
 
+
+
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Step5AppTheme() {
-                    AuthScreen()
+            val viewModel: SettingsViewModel = hiltViewModel()
+            val uiState by viewModel.uiState.collectAsState()
+
+            Step5AppTheme(selectedTheme = uiState.themePreference.lowercase()) {
+                MainNavHost()
             }
         }
     }

7d249d3cabbf1b0ee57ff1ce53a547551801e629
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-05-22 23:54:28 +0300

    extract some general components to use it again

---
+++ app/src/main/java/com/example/step5app/presentation/common/components.kt
@@ -0,0 +1,277 @@
+package com.example.step5app.presentation.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.step5app.R
+
+@Composable
+fun SectionTitle(title: String) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.titleMedium,
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colorScheme.onSurface,
+        modifier = Modifier.padding(vertical = 4.dp),
+        fontSize = 18.sp
+    )
+    HorizontalDivider(
+        modifier = Modifier.width((title.length.toFloat() * 13f - 3 * kotlin.math.sqrt(title.length.toFloat())).dp),
+        thickness = 3.dp,
+        color = MaterialTheme.colorScheme.tertiary
+    )
+}
+
+@Composable
+fun ContactRow(icon: Int, text: String) {
+    Row(
+        verticalAlignment =
+            Alignment.CenterVertically,
+        modifier = Modifier.padding(top = 22.dp, )) {
+        Icon(
+            painterResource(icon),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .scale(1.2f)
+                .padding(end = 8.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+            Text(
+                text = text,
+                color = MaterialTheme.colorScheme.onSurface,
+                style = TextStyle.Default.copy(textDirection = TextDirection.Ltr),
+                fontSize = 16.sp
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ThemeToggleRow(text: String, icon: Int, selected: Boolean, onClick: () -> Unit) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(vertical = 2.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painterResource(icon),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.scale(1.2f)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text, color = MaterialTheme.colorScheme.onSurface, modifier = Modifier
+            .weight(1f)
+            .padding(start = 5.dp), fontSize = 14.sp)
+        Switch(
+            checked = selected,
+            onCheckedChange = { onClick() },
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = MaterialTheme.colorScheme.secondaryContainer,
+                uncheckedThumbColor = MaterialTheme.colorScheme.surfaceVariant,
+                checkedTrackColor = MaterialTheme.colorScheme.onTertiaryContainer, // Bright green
+                uncheckedTrackColor = MaterialTheme.colorScheme.tertiaryContainer,
+                checkedBorderColor = MaterialTheme.colorScheme.onTertiaryContainer, // To give a visible border
+                uncheckedBorderColor = Color.Transparent,
+            ),
+            modifier = Modifier.scale(0.8f) // Optional: Slightly shrink switch to match the compact look
+        )
+    }
+}
+
+@Composable
+fun LanguageToggleRow(language: String, countryCode: String, selected: Boolean, onClick: () -> Unit) {
+    val flag = when (countryCode) {
+        "US" -> "\uD83C\uDDFA\uD83C\uDDF8" // 🇺🇸
+        "SA" -> "\uD83C\uDDF8\uD83C\uDDE6" // 🇸🇦
+        else -> ""
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 26.dp)
+            .border(
+                width = 1.dp,
+                color = if (selected) MaterialTheme.colorScheme.onTertiaryContainer else Color.Transparent
+            )
+            .background(if (selected) MaterialTheme.colorScheme.onTertiaryContainer.copy(alpha = 0.3f) else MaterialTheme.colorScheme.surface)
+            .clickable { onClick() }
+            .padding(8.dp)
+
+    ) {
+        Text(
+            text = "$flag $language",
+            color = if (selected) MaterialTheme.colorScheme.onTertiaryContainer else MaterialTheme.colorScheme.onSurface,
+        )
+    }
+}
+
+
+@Composable
+fun NameFieldsRow(
+    firstName: String,
+    onFirstNameChange: (String) -> Unit,
+    lastName: String,
+    onLastNameChange: (String) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        OutlinedTextField(
+            value = firstName,
+            onValueChange = onFirstNameChange,
+            label = {
+                Text(
+                    text = stringResource(R.string.first_name),
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            },
+            modifier = Modifier.weight(1f),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+                unfocusedContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+                disabledContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+            )
+        )
+
+        OutlinedTextField(
+            value = lastName,
+            onValueChange = onLastNameChange,
+            label = {
+                Text(
+                    text = stringResource(R.string.last_name),
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            },
+            modifier = Modifier.weight(1f),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+                unfocusedContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+                disabledContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+            )
+        )
+    }
+}
+
+
+@Composable
+fun EmailField(
+    email: String,
+    onEmailChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        value = email,
+        onValueChange = onEmailChange,
+        label = {
+            Text(
+                text = stringResource(R.string.email),
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        },
+        leadingIcon = {
+            Icon(
+                painter = painterResource(R.drawable.mail),
+                contentDescription = null,
+                modifier = Modifier.size(20.dp),
+                tint = MaterialTheme.colorScheme.onSurface
+            )
+        },
+        modifier = modifier.fillMaxWidth(),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+            unfocusedContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+            disabledContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+        )
+    )
+}
+
+
+
+@Composable
+fun PasswordField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    onVisibilityToggle: () -> Unit,
+    isVisible: Boolean,
+    label: String,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = {
+            Text(
+                text = label,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        },
+        leadingIcon = {
+            Icon(
+                painter = painterResource(R.drawable.lock),
+                contentDescription = null,
+                modifier = Modifier.size(20.dp),
+                tint = MaterialTheme.colorScheme.onSurface
+            )
+        },
+        trailingIcon = {
+            IconButton(onClick = onVisibilityToggle) {
+                Icon(
+                    painter = painterResource(R.drawable.eye),
+                    contentDescription = null,
+                    modifier = Modifier.size(20.dp),
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        },
+        modifier = modifier.fillMaxWidth(),
+        visualTransformation = if (isVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+            unfocusedContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+            disabledContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+        )
+    )
+}
+
+

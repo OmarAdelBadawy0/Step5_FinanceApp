1e24a88b96e602f6f9f773710462f6f894103388
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-05-24 01:22:33 +0300

    the correct way to use final navController and ThemePreference

--- app/src/main/java/com/example/step5app/MainActivity.kt
+++ app/src/main/java/com/example/step5app/MainActivity.kt
@@ -1,23 +1,17 @@
 package com.example.step5app
 
-import FeedScreen
-import SettingsScreen
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.step5app.presentation.auth.AuthScreen
-import com.example.step5app.presentation.navigation.MainNavHost
 import com.example.step5app.presentation.settings.SettingsViewModel
 import com.example.step5app.ui.theme.Step5AppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 import androidx.compose.runtime.getValue
-
-
+import com.example.step5app.presentation.navigation.MainNavHost
+import com.example.step5app.presentation.profile.ProfileScreen
 
 
 @AndroidEntryPoint
@@ -29,7 +23,9 @@ class MainActivity : ComponentActivity() {
             val viewModel: SettingsViewModel = hiltViewModel()
             val uiState by viewModel.uiState.collectAsState()
 
-            Step5AppTheme(selectedTheme = uiState.themePreference.lowercase()) {
+            Step5AppTheme(
+                selectedTheme = uiState.themePreference.lowercase()
+            ) {
                 MainNavHost()
             }
         }

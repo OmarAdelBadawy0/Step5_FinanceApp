6ad6264f1819a71655e8b276d3fb5ac35f05eb37
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-05-24 01:11:37 +0300

    modify the BottomBar to take the nav controller and make the navigation correctly

--- app/src/main/java/com/example/step5app/presentation/bottomBar/BottomBarViewModel.kt
+++ app/src/main/java/com/example/step5app/presentation/bottomBar/BottomBarViewModel.kt
@@ -3,8 +3,10 @@ package com.example.step5app.presentation.bottomBar
 import androidx.compose.runtime.IntState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class BottomBarViewModel @Inject constructor() : ViewModel() {
     private val _selectedTab = mutableIntStateOf(0)
     val selectedTab: IntState = _selectedTab

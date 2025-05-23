d8e2ce891f1d9435b364296b20957856fe69c80d
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-05-24 01:06:17 +0300

    add the new routs of Profile, auth, and all in BottomBar

--- app/src/main/java/com/example/step5app/presentation/navigation/NavRoutes.kt
+++ app/src/main/java/com/example/step5app/presentation/navigation/NavRoutes.kt
@@ -1,7 +1,14 @@
 package com.example.step5app.presentation.navigation
 
 sealed class Screen(val route: String) {
-    object Auth : Screen("auth")  // This will contain both sign-in/sign-up
+    object AuthR : Screen("auth_root")  // This will contain both sign-in/sign-up
+    object Auth : Screen("auth")
+    object MainApp : Screen("main_app")
+    object SignIn : Screen("sign_in")
+    object SignUp : Screen("sign_up")
     object Feed : Screen("feed")  // Renamed from Home to Feed for clarity
     object Settings : Screen("settings")
+    object Courses : Screen("courses")
+    object Network : Screen("network")
+    object Profile : Screen("profile")
 }

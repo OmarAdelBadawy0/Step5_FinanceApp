7026a253f85b53dc382abe975a8ed5dd69ae027c
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-05-31 00:28:02 +0300

    add the NotificationScreen call

--- app/src/main/java/com/example/step5app/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/step5app/presentation/profile/ProfileScreen.kt
@@ -16,20 +16,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -42,10 +36,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -67,7 +57,7 @@ import com.example.step5app.presentation.common.PasswordField
 import com.example.step5app.presentation.common.SectionTitle
 import com.example.step5app.presentation.deleteAccount.DeleteAccountSection
 import com.example.step5app.presentation.myCourses.MyCoursesScreen
-import com.example.step5app.presentation.navigation.Screen
+import com.example.step5app.presentation.notifications.NotificationScreen
 import com.example.step5app.presentation.subscription.SubscriptionPlan
 
 @Composable
@@ -312,7 +302,10 @@ fun ProfileScreen(
             }
             else if (uiState.selectedTabIndex == 2) {
                 SubscriptionPlan()
-            }else if (uiState.selectedTabIndex == 4) {
+            }else if (uiState.selectedTabIndex == 3) {
+                NotificationScreen()
+            }
+            else if (uiState.selectedTabIndex == 4) {
                 DeleteAccountSection(
                     onDeleteClick = { /*TODO*/ },
                     onSendCodeClick = { /*TODO*/ },

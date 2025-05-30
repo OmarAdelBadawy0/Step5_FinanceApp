28fc1028f7f55264a8e84e18d16e046ce93f545b
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-05-31 00:30:27 +0300

    delete hashed unnecessary part

--- app/src/main/java/com/example/step5app/presentation/navigation/MainNavHost.kt
+++ app/src/main/java/com/example/step5app/presentation/navigation/MainNavHost.kt
@@ -1,21 +1,9 @@
 package com.example.step5app.presentation.navigation
 
-import FeedScreen
-import SettingsScreen
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.step5app.presentation.auth.AuthScreen
-import com.example.step5app.presentation.feed.FeedViewModel
-import com.example.step5app.presentation.settings.SettingsViewModel
 
 @Composable
 fun MainNavHost(
@@ -31,16 +19,6 @@ fun MainNavHost(
         authGraph(navController, onAuthComplete = {})
         mainGraph(navController)
 
-//        authGraph(navController) {
-//            navController.navigate("main_root") {
-//                popUpTo(navController.graph.findStartDestination().id) {
-//                    saveState = true
-//                }
-//                launchSingleTop = true
-//                restoreState = true
-//            }
-//        }
-//
-//        mainGraph(navController)
+
     }
 }

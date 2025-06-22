824a4e5ad6713f13daec32599b47f8897ea65be3
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-06-22 20:09:20 +0300

    update card offset to optimize all devices screens for logo and card

--- app/src/main/java/com/example/step5app/presentation/auth/AuthScreen.kt
+++ app/src/main/java/com/example/step5app/presentation/auth/AuthScreen.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -44,12 +45,16 @@ import com.example.step5app.presentation.auth.sign_up.SignUpFields
 @Composable
 fun AuthScreen(onSignInSuccess: () -> Unit) {
     var isSignUp by remember { mutableStateOf(false) }
+
+    val screenHeight = LocalWindowInfo.current.containerSize.height.dp
+
     val cardOffset by animateDpAsState(
-        targetValue = if (isSignUp) (-600).dp else 0.dp,
-        animationSpec = tween(durationMillis = 700, easing = LinearOutSlowInEasing	)
+        targetValue = if (isSignUp) (screenHeight.value * -0.235f).dp else (screenHeight.value * 0.0240f).dp,
+        animationSpec = tween(durationMillis = 700, easing = LinearOutSlowInEasing)
     )
+
     val logoOffset by animateDpAsState(
-        targetValue = if (isSignUp) (750).dp else (30).dp,
+        targetValue = if (isSignUp) (screenHeight.value * 0.305f).dp else (screenHeight.value * -0.0160f).dp,
         animationSpec = tween(durationMillis = 700, easing = LinearOutSlowInEasing)
     )
 

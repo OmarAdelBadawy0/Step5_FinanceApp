5efc577c6a649238df36aaa37fbe25f991bf5c95
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-06-29 03:51:00 +0300

    create the data class of auth response

--- app/src/main/java/com/example/step5app/data/model/AuthResponse.kt
+++ app/src/main/java/com/example/step5app/data/model/AuthResponse.kt
@@ -1,7 +1,9 @@
 package com.example.step5app.data.model
 
+
 data class AuthResponse(
     val success: Boolean,
-    val token: String? = null,
-    val error: String? = null
+    val message: List<String>?,
+    val error: String?,
+    val statusCode: Int?
 )

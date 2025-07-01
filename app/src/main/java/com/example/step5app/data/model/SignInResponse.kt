6037fae6c0fe7b5442221f99be91b0aa05b63ac4
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-07-02 01:00:04 +0300

    create the response object

---
+++ app/src/main/java/com/example/step5app/data/model/SignInResponse.kt
@@ -0,0 +1,8 @@
+package com.example.step5app.data.model
+
+data class SignInResponse(
+    val message: String,
+    val data: TokenData?,
+    val pagination: Any?
+
+)
---
+++ app/src/main/java/com/example/step5app/data/model/TokenData.kt
@@ -0,0 +1,5 @@
+package com.example.step5app.data.model
+
+data class TokenData(
+    val accessToken: String
+)

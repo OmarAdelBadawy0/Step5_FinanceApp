5faf138b1f88c51f70f880b6586c62636089e7f2
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-07-04 15:33:24 +0300

    add the plan models

---
+++ app/src/main/java/com/example/step5app/domain/model/Plan.kt
@@ -0,0 +1,11 @@
+package com.example.step5app.domain.model
+
+data class Plan(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val price: Double,
+    val annualDiscount: Double,
+    val createdAt: String,
+    val updatedAt: String
+)
--- app/src/main/java/com/example/step5app/domain/model/Subscription.kt
+++
@@ -1,9 +0,0 @@
-package com.example.step5app.domain.model
-
-data class Subscription(
-    val name: String,
-    val price: Double,
-    val duration: String,
-    val features: List<String>,
-    val isSubscribed: Boolean
-)

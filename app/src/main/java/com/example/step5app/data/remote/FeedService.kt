4a0dc04dc260460f23e6bdbd2a347f97e4729a80
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-07-20 23:37:27 +0300

    add a getPosts service to call the articles endpoint

--- app/src/main/java/com/example/step5app/data/remote/FeedService.kt
+++ app/src/main/java/com/example/step5app/data/remote/FeedService.kt
@@ -1,9 +1,11 @@
 package com.example.step5app.data.remote
 
 import com.example.step5app.data.model.CategoriesResponse
+import com.example.step5app.data.model.PostResponse
 import com.example.step5app.domain.model.Category
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface FeedService {
     @GET("/categories")
@@ -11,4 +13,14 @@ interface FeedService {
         @Header("Accept-Language") language: String = "en",
         @Header("Authorization") token: String
     ): CategoriesResponse<List<Category>>
+
+
+    @GET("/articles")
+    suspend fun getPosts(
+        @Query("categoryId") categoryId: Int? = null,
+        @Query("search") search: String? = null,
+        @Query("page") page: Int = 1,
+        @Query("limit") limit: Int = 5,
+        @Header("Accept-Language") language: String = "en"
+    ): PostResponse
 }

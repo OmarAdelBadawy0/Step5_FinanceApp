aa92ce5ab8efd836649f75882cfc85b1888a548c
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-07-24 22:58:31 +0300

    Enable search confirmation in TopBar
    This commit modifies the `TopBar` composable to trigger a search when the user presses the search button on the keyboard.

--- app/src/main/java/com/example/step5app/presentation/topBar/TopBar.kt
+++ app/src/main/java/com/example/step5app/presentation/topBar/TopBar.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,6 +16,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -25,7 +28,8 @@ import com.example.step5app.R
 fun TopBar(
     viewModel: TopBarViewModel = hiltViewModel(),
     onSettingsClick: () -> Unit = {},
-    withSearch: Boolean = false
+    withSearch: Boolean = false,
+    onConfirmClick: (String) -> Unit = {}
 ) {
     val searchText by viewModel.searchText
 
@@ -95,6 +99,12 @@ fun TopBar(
                             unfocusedTextColor = MaterialTheme.colorScheme.onSurface
                         ),
                         singleLine = true,
+                        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+                        keyboardActions = KeyboardActions(
+                            onSearch = {
+                                onConfirmClick(searchText)
+                            }
+                        )
                     )
                 }
             },

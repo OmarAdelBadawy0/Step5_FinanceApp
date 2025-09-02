87e80faf736b04b3a0ad98bd9c596c161e4d96df
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2025-09-03 00:55:18 +0300

    feat: implement make subscription functionality
    Key changes:
    - Added `makeSubscription` endpoint to `PlanService` to handle subscription requests.
    - Implemented `makeSubscription` in `PlanRepository` to call the service and handle the response.

--- app/src/main/java/com/example/step5app/data/remote/PlanService.kt
+++ app/src/main/java/com/example/step5app/data/remote/PlanService.kt
@@ -1,13 +1,24 @@
 package com.example.step5app.data.remote
 
+import com.example.step5app.data.local.SubscriptionRequest
+import com.example.step5app.data.local.SubscriptionResponse
 import com.example.step5app.data.model.PlanResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface PlanService {
     @GET("/plans")
     suspend fun getPlans(
         @Header("Accept-Language") lang: String = "en"
     ): Response<PlanResponse>
+
+    @POST("subscriptions")
+    suspend fun makeSubscription(
+        @Header("Accept-Language") lang: String = "en",
+        @Header("Authorization") token: String,
+        @Body request: SubscriptionRequest
+    ): Response<SubscriptionResponse>
 }
--- app/src/main/java/com/example/step5app/data/repositories/PlanRepository.kt
+++ app/src/main/java/com/example/step5app/data/repositories/PlanRepository.kt
@@ -1,11 +1,16 @@
 package com.example.step5app.data.repositories
 
+import android.util.Log
+import com.example.step5app.data.local.SubscriptionRequest
+import com.example.step5app.data.local.SubscriptionResponse
+import com.example.step5app.data.local.UserPreferences
 import com.example.step5app.data.remote.PlanService
 import com.example.step5app.domain.model.Plan
 import javax.inject.Inject
 
 class PlanRepository @Inject constructor(
-    private val planService: PlanService
+    private val planService: PlanService,
+    private val userPreferences: UserPreferences
 ) {
     suspend fun fetchPlans(): List<Plan> {
         val response = planService.getPlans()
@@ -15,4 +20,16 @@ class PlanRepository @Inject constructor(
             throw Exception("Failed to fetch plans")
         }
     }
+
+    suspend fun makeSubscription(request: SubscriptionRequest): SubscriptionResponse {
+        val token = userPreferences.getAccessTokenOnce()
+        val response = planService.makeSubscription(token = "Bearer $token", request = request)
+        Log.d("SubscriptionResponse", response.message())
+        if (response.isSuccessful) {
+            return response.body() ?: throw Exception("Failed to make subscription")
+        } else {
+            throw Exception(response.code().toString())
+        }
+    }
+
 }
